803c25175d15525c2d8cebfbf1b9280401869a8a
jessica <jsimao2@ggc.edu>
2023-10-01 12:07:58 -0400

    firebase initialized

--- app.js
+++ app.js
@@ -1,12 +1,5 @@
-// Import the Firebase SDK
-import firebase from 'firebase/app';
-import 'firebase/auth'; // If you need authentication
-import 'firebase/firestore'; // If you need Firestore
-import 'firebase/database'; // If you need Realtime Database
-// Add other Firebase services as needed
-
+// Initialize Firebase with your Firebase project configuration
 const firebaseConfig = {
-
     apiKey: "AIzaSyBYRGv9ywL0jo-pmFb5kGT2R9nDZYmSd0c",
     authDomain: "todolist-5c9c3.firebaseapp.com",
     databaseURL: "https://todolist-5c9c3-default-rtdb.firebaseio.com",
@@ -15,52 +8,51 @@ const firebaseConfig = {
     messagingSenderId: "745955379661",
     appId: "1:745955379661:web:89b10e17d9a73b37e65c84",
     measurementId: "G-EGJZKGL0XG"
-  
   };
   
-  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const db = firebase.firestore();
+const database = firebase.database();
+const taskRef = database.ref("tasks");
 
+const taskInput = document.getElementById("taskInput");
+const addTaskBtn = document.getElementById("addTask");
+const taskList = document.getElementById("taskList");
 
-// Function to add a new task
+// Function to add a new task to Firebase
 function addTask() {
-    const taskInput = document.getElementById("task");
     const taskText = taskInput.value.trim();
-
     if (taskText !== "") {
-        // Add the task to Firestore
-        db.collection("tasks").add({
+        const newTaskRef = taskRef.push();
+        newTaskRef.set({
             text: taskText,
-            timestamp: firebase.firestore.FieldValue.serverTimestamp()
-        })
-        .then(() => {
-            taskInput.value = "";
-        })
-        .catch(error => {
-            console.error("Error adding task: ", error);
+            completed: false
         });
+        taskInput.value = "";
     }
 }
 
-// Function to display tasks from Firestore
-function displayTasks() {
-    const taskList = document.getElementById("taskList");
-
-    db.collection("tasks")
-        .orderBy("timestamp", "desc")
-        .onSnapshot(snapshot => {
-            taskList.innerHTML = "";
-            snapshot.forEach(doc => {
-                const taskData = doc.data();
-                const taskItem = document.createElement("li");
-                taskItem.textContent = taskData.text;
-                taskList.appendChild(taskItem);
-            });
+// Function to display tasks from Firebase
+function displayTasks(snapshot) {
+    const tasks = snapshot.val();
+    taskList.innerHTML = "";
+    for (const taskId in tasks) {
+        const task = tasks[taskId];
+        const taskItem = document.createElement("li");
+        taskItem.innerHTML = `
+            <input type="checkbox" ${task.completed ? "checked" : ""}>
+            <span>${task.text}</span>
+        `;
+        taskItem.querySelector("input").addEventListener("change", (e) => {
+            const updatedTaskRef = taskRef.child(taskId);
+            updatedTaskRef.update({ completed: e.target.checked });
         });
+        taskList.appendChild(taskItem);
+    }
 }
 
-// Call the displayTasks function to initially load tasks
-displayTasks();
+// Event listener for adding a new task
+addTaskBtn.addEventListener("click", addTask);
+
+// Event listener for displaying tasks when the page loads
+taskRef.on("value", displayTasks);
